d113a3d6d262559d6945a91503e900e998c6ff40
Leiyi548 <1424630446@qq.com>
2022-01-25 17:14:23 +0800

    update

--- C++_back/a.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-int main() {
-  cout<<"hello,wolrd"<<"\n";
-  cout<<"This is not a problem!"<<endl;
-  return 0;
-}
--- C++_back/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char *argv[])
-{
-  printf("你好啊");
-  return 0;
-}
